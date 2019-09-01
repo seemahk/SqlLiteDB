9206e26fa6b11e64801c687558e465952e4e9dcf
seema kale <seemakale216@gmail.coma>
2019-08-31 17:28:53 -0700

    Test

--- app/src/main/java/com/example/sqllitedb/MainActivity.java
+++ app/src/main/java/com/example/sqllitedb/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.sqllitedb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -159,6 +163,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_set:
+                startActivity(new Intent(Settings.ACTION_SETTINGS));
+                break;
+            case R.id.menu_wait:
+                AlertDialog.Builder builder=new AlertDialog.Builder(this);
+                builder.setTitle("Keep Waiting");
+                builder.setMessage("Keep smiling");
+                builder.setCancelable(true);
+                builder.show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
